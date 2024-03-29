8f1ba6f2fc274ef28c5e8e6dd4cb76094a841ea1
George Davidson <georgedavidson392@gmail.com>
2024-03-29 13:15:44 -0400

    from vs to vsc

---
+++ player.h
@@ -0,0 +1,31 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "tile.h"
+#include <sfml/Graphics.hpp>
+
+class Player {
+  public:
+    Player(float speed, sf::Color color, sf::Vector2f size, sf::Vector2f pos);
+
+    void update(sf::RenderWindow &window, float deltaTime, std::vector<Tile> &tileGroup);
+
+    void draw(sf::RenderWindow &window);
+
+  private:
+    float speed;
+
+    sf::Vector2f direction;
+    sf::RectangleShape rect;
+
+    void horizontalMovement(float deltaTime);
+
+    void horizontalCollisions(std::vector<Tile> &tileGroup);
+
+    void verticalMovement(float deltaTime);
+
+    void verticalCollisions(std::vector<Tile> &tileGroup);
+
+    void camera(sf::RenderWindow &window);
+};
+#endif
